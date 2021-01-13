67a167b5da58ea31c6a3aa313a19296de2eefa79
Miho-12 <mihoo12@hotmail.com>
2021-01-13 15:11:31 +0100

    First commit
    Added Unity project to Github

---
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] List<WaveConfig> waveConfigs;
+    [SerializeField] int startingWave = 0;
+    [SerializeField] bool looping = false;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        do
+        {
+            yield return StartCoroutine(SpawnAllWaves());
+        }
+        while (looping);
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        for(int waveCountIndex = startingWave; waveCountIndex < waveConfigs.Count; waveCountIndex++)
+        {
+            var currentWave = waveConfigs[waveCountIndex];
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+
+        }
+    }
+
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int enemyCountIndex = 0; enemyCountIndex < waveConfig.GetNumberOfEnemies(); enemyCountIndex++)
+        {
+            var newEnemy = Instantiate(
+                waveConfig.GetEnemyPrefab(),
+                waveConfig.GetWaypoints()[0].transform.position,
+                Quaternion.identity);
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+    }
+}
---
+++ Laser Defender/Assets/Scripts/Level.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+
+    [SerializeField] float delayInSeconds = 2f;
+    [SerializeField] float kesleltetes = 2.5f;
+    AudioClip kispirics;
+    [SerializeField] [Range(0, 1)] float kispiricsVolume = 0.75f;
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void PlayKispirics()
+    {
+        if(SceneManager.GetActiveScene().name == "Game")
+        {
+            AudioSource.PlayClipAtPoint(kispirics, Camera.main.transform.position, kispiricsVolume);
+        }
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+
+
+public void LoadGameOver()
+    {
+        StartCoroutine(WaitAndLoad());
+    }
+
+    IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+       SceneManager.LoadScene("Game Over");
+    }
+
+    public void QuitGame()
+    {
+    #if UNITY_EDITOR
+
+        // Application.Quit() does not work in the editor so
+
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+
+        UnityEditor.EditorApplication.isPlaying = false;
+
+    #else
+
+         Application.Quit();
+
+    #endif
+ 
+    }
+
+}
